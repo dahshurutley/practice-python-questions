a6a83bf6fa9f395bd794618930877dc16f43c3c2
dahshurutley <dahshurutley@gmail.com>
2021-12-27 15:36:57 -0500

    03: Updated

--- solutions-extras/03:List-Less-Than-Ten-Extras.py
+++ solutions-extras/03:List-Less-Than-Ten-Extras.py
@@ -5,11 +5,7 @@ print("Finding integers less than the given number within a list: ")
 
 def lessthan():
     print('Numbers Less than 5: ')
-    for i in a: 
-        if i < 5:
-            b.append(i)
-    
-    print(b)
+    print([i for i in a if i < 5])
     print('')
     manager()
 
@@ -18,12 +14,7 @@ def userIn():
     
     numIn = int(input("Please Input a Number: "))
     print(f'Numbers Less than {numIn}: ')
-    c = []
-    for i in a:
-        if i < numIn:
-            c.append(i)
-
-    print(c)
+    print([i for i in a if i < numIn])
     print('')
     manager()
             
