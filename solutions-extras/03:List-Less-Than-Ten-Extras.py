d923493e6de7a8fda0695d43a6f084fcaf3efbe0
dahshurutley <dahshurutley@gmail.com>
2021-12-27 15:42:04 -0500

    03: Updated

--- solutions-extras/03:List-Less-Than-Ten-Extras.py
+++ solutions-extras/03:List-Less-Than-Ten-Extras.py
@@ -1,14 +1,11 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = []
 
 print("Finding integers less than the given number within a list: ")
-
 def lessthan():
     print('Numbers Less than 5: ')
     print([i for i in a if i < 5])
     print('')
     manager()
-
 
 def userIn():
     
@@ -18,14 +15,11 @@ def userIn():
     print('')
     manager()
             
-
 def manager(): 
 
     userInput = str(input("'M' to enter user version \n'N' To Enter normal version\n'Enter' to exit: ")).capitalize()
-
     if userInput == 'M':
         userIn()
-    
     if userInput == 'N':
         lessthan()
 
