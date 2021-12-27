da859a3dee565fe8f4af631c2b4442b77fc0446d
dahshurutley <dahshurutley@gmail.com>
2021-12-27 01:18:39 -0500

    01

--- solutions/01:Character-Input.py
+++ solutions/01:Character-Input.py
@@ -1,1 +1,18 @@
+from datetime import datetime
 
+def whiteyear():
+
+    a = datetime.now(tz=None)
+    b = a.year
+
+    userName = str(input('Please Enter Your Name: '))
+    userAge = int(input('What Is Your Age?: '))
+
+    print(f"You will turn 100 in {100 - userAge + b}")
+
+whiteyear()
+
+
+
+
+    
