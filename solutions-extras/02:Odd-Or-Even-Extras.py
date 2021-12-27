a2ec23fccc600c4fe0db97780aca2bcd8b40a260
dahshurutley <dahshurutley@gmail.com>
2021-12-27 16:41:41 -0500

    02:Updated

--- solutions-extras/02:Odd-Or-Even-Extras.py
+++ solutions-extras/02:Odd-Or-Even-Extras.py
@@ -1,3 +1,9 @@
+# Ask the user for a number. 
+# Depending on whether the number is even or odd, print out an appropriate message to the user.
+# If the number is a multiple of 4, print out a different message.
+# Ask the user for two numbers: one number to check (call it num) and one number to divide by (check).
+# If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+
 num = int(input('Please Enter a number: '))
 check = int(input(f'Please input number you want {num} to be divided by: '))
 
--- solutions/02:Odd-Or-Even.py
+++ solutions/02:Odd-Or-Even.py
@@ -1,3 +1,6 @@
+# Ask the user for a number. 
+# Depending on whether the number is even or odd, print out an appropriate message to the user.
+
 a = int(input('Please Enter a number: '))
 
 if a % 2 == 0: 
