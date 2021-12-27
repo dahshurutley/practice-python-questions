eef90fc4101f22c0c4b4b06b1329831ad499cf79
dahshurutley <dahshurutley@gmail.com>
2021-12-27 16:27:01 -0500

    02: Updated

--- solutions/02:Odd-Or-Even.py
+++ solutions/02:Odd-Or-Even.py
@@ -1,8 +1,10 @@
 a = int(input('Please Enter a number: '))
 
-if a % 2 != 0: 
+if a % 2 == 0: 
+    print('The Number is Even!')
+else:
     print('The Number is Odd!')
 
 # Extra 1 in this solution
-elif a % 4 == 0:
+if a % 4 == 0:
     print('The number is a multiple of 4! (and therefore even)')
