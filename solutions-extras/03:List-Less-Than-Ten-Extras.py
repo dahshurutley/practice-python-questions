7d921929dcb7d2406cf7491b99729a36bf96c3b1
dahshurutley <dahshurutley@gmail.com>
2021-12-27 16:47:19 -0500

    03:Updated

--- solutions-extras/03:List-Less-Than-Ten-Extras.py
+++ solutions-extras/03:List-Less-Than-Ten-Extras.py
@@ -1,3 +1,9 @@
+# Take a list and write a program that prints out all the elements of the list that are less than 5.
+# Make a new list that has all the elements less than 5 from this list in it and print out this new list.
+# Write this in one line of Python.
+# Ask the user for a number and return a list that contains only elements from the original list...
+# ...That are smaller than that number given by the user.
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 print("Finding integers less than the given number within a list: ")
