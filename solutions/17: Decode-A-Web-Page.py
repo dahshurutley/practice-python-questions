b16b9376d60dad324a6831b8d3b2c6f53a23bf9f
dahshurutley <dahshurutley@gmail.com>
2022-01-03 15:09:24 -0500

    017: General Updates

--- solutions/17: Decode-A-Web-Page.py
+++ solutions/17: Decode-A-Web-Page.py
@@ -1,3 +1,28 @@
-# Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the New York Times\
+# Use the BeautifulSoup and requests Python packages
+# Print out a list of all the article titles on the New York Times
+
+import requests 
+from bs4 import BeautifulSoup
+from lxml import html
+
+# Note: Use soup.get-all-text to return all text from the article
+# Find a way to export website as html, format it, then print text. 
+
+url = 'http://www.nytimes.com/'
+r = requests.get(url)
+# Personal Note: r.text displays the websites HTML
+r_html = r.text
+
+soup = BeautifulSoup(r_html, 'html.parser')
+
+
+# For loop finds each h3 value and gets text from each said value. 
+for i in soup.find_all("h3", {"class": "e1lsht870"}):
+    
+    print(i.getText())
+
+
+    
+
 
 
