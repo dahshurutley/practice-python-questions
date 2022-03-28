e15cb8390ec1f8ab952eeb2a307a4bae50a1782d
Dahshur Utley <87342746+dahshurutley@users.noreply.github.com>
2022-03-28 05:15:43 +0000

    UPDATES

--- solutions/18_Cows-And-Bulls.py
+++ solutions/18_Cows-And-Bulls.py
@@ -1,10 +1,9 @@
 # Create a program that will play the “cows and bulls” game with the user. The game works like this:
 
-# Randomly generate a 4-digit number. Ask the user to guess a 4-digit number. For every digit that the user guessed correctly in the correct place, they have a “cow”. For every digit the user guessed correctly in the wrong place is a “bull.” Every time the user makes a guess, tell them how many “cows” and “bulls” they have. Once the user guesses the correct number, the game is over. Keep track of the number of guesses the user makes throughout teh game and tell the user at the end.
+# Randomly generate a 4-digit number. Ask the user to guess a 4-digit number. For every digit that the user guessed correctly in the correct place, they have a “cow”. For every digit the user guessed correctly in the wrong place is a “bull.” # Every time the user makes a guess, tell them how many “cows” and “bulls” they have. Once the user guesses the correct number, the game is over. Keep track of the number of guesses the user makes throughout the game and tell the user at the end.
 
 
 import random
-import sys
 # Cow = Correct Placement 
 # Bull = Wrong Place Correct Guess
 NUM = random.randint(1000, 10000)
@@ -20,24 +19,32 @@ counter = 0
 while True: 
   user_input = str(input('Guess a 4 digit number: '))
   guess  = [i for i in user_input]
+  print(guess)
   counter += 1
   
+  if user_input == ''.join(SOLUTION):
+      print(f'Correct, the number was {"".join(SOLUTION)}, it took you {counter} tries!')
+      break 
+  if len(user_input) != 4: 
+      print('Not a 4 digit number')
+      break
+
+  # Evaluator
   for i in guess: 
       if i in SOLUTION:
         if guess.index(i) == SOLUTION.index(i):
-          cow += 1
-          print(cow)
+          if guess.count(i) > 1:
+            cow += 2
+            cow -= 1
+          else: 
+            cow += 1
         if guess.index(i) != SOLUTION.index(i):
-          bull += 1
-          print(bull)
-      if user_input == ''.join(SOLUTION):
-        if counter == 1:
-            print('You Have 4 cows!')
-            print(f'You have {bull} bulls!')
-        else: 
-          print(f'You Have {cow} cows!')
-          print(f'You have {bull} bulls!')
-        sys.exit()
+          bull += 1 
         
-        
+  print(f'You have {cow} cows and {bull} bulls.')
+  bull = bull - bull
+  cow = cow - cow
+    
+  
+         
         
